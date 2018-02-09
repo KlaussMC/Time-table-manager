83365e104ed0033f2108c3f6378e7620ca994891
Jacob <jakieschneider13@gmail.com>
2018-02-09 11:59:33 +1100

    Built login/signin page
    The main bulk of the front end is done, now we need to build the backend and the homepage, we cannot continue with the main program before the backend is complete.

---
+++ 7-days.io/7-days.io/routes/login.js
@@ -0,0 +1,8 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+  res.render('login', { title: '7-days.io' });
+});
+module.exports = router;
