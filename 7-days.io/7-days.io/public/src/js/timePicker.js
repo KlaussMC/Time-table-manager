9640b503dbcec1dcd3799afc285f1ebc56cd32a5
KlaussMC <jakieschneider13@gmail.com>
2018-02-23 19:17:28 +1100

    Build main bulk of editor
    Fine tuning to be done and some corner-rounding, as well as the submission and the last final features of the editor such as changing the size of the table and seeing the changes live.

---
+++ 7-days.io/7-days.io/public/src/js/timePicker.js
@@ -0,0 +1,36 @@
+﻿let hrs = null;
+let mns = null
+
+let objct;
+
+function pickTime(obj) {
+  objct = obj;
+  document.querySelector(".prompt").style.display = "block";
+  let items = "";
+  for (let i = 1; i <= 12; i++) {
+    let interval = 360 / 12;
+    let degree = interval * i;
+
+    let val = (hrs != null) ? ((i == 12) ? 00 : i * 5) : i;
+    items += `<span class="deg${degree}" onclick="action(this)">${val}</span>`;
+  }
+  document.querySelector(".clock").innerHTML = `<div class="time">${items}</div>`;
+}
+const hrsSet = obj => hrs = Math.round(obj.innerHTML);
+const mnsSet = obj => mns = Math.round(obj.innerHTML);
+const action = obj => { if (hrs == null) { step2(obj) } else { finish(obj) } };
+
+const setTime = obj => obj.innerHTML = hrs + ":" + mns;
+
+function step2(obj) {
+  hrsSet(obj);
+  pickTime(objct);
+}
+function finish(obj) {
+  mnsSet(obj);
+  setTime(objct);
+  document.querySelector(".prompt").style.display = "none";
+
+  hrs = null;
+  mns = null;
+}
--- 7-days.io/7-days.io/public/src/js/ttmanager.js
+++ 7-days.io/7-days.io/public/src/js/ttmanager.js
@@ -2,10 +2,29 @@
 
 let data = [];
 
-const editField = (field) => { let dat = {}; /* get day and period */ data.push(getInfo(dat)) }
+let field;
 
-const getInfo = (field) => {
+function editField(block) {
+  field = block;
+  document.querySelector(".getData").style.display = "block";
+}
+function parseData() {
   let dat = {
-    day: 
+    name: document.querySelector("#name").value,
+    from: document.querySelector("#from").innerHTML,
+    to: document.querySelector("#to").innerHTML,
+    loc: document.querySelector("#where").value
   }
+  return dat;
+}
+
+const confirmEvent = () => {
+  // set value of innerHTML of field to recieved data
+  let data = parseData();
+
+  field.innerHTML = "<B>" + data.name + "</B>" + "<br>" + data.loc;
+  field.setAttribute("from", data.from)
+  field.setAttribute("to", data.to)
+
+  document.querySelector(".getData").style.display = "none";
 }
