0f2da5f286b2cdb3753f2ca8984b7d55bcbafc8d
KlaussMC <jakieschneider13@gmail.com>
2018-02-24 14:14:11 +1100

    made editor functional
    all that's left to be done is finish up the time-shifting system

---
+++ 7-days.io/7-days.io/public/src/js/timeKeeper.js
@@ -0,0 +1,51 @@
+﻿function addTimes(time1, time2) {
+  let newTime = 0;
+
+  let t1 = time1.split(":");
+  let t2 = time2.split(":");
+
+  t1[0] = Math.round(t1[0])
+  t1[1] = Math.round(t1[1])
+  t2[0] = Math.round(t2[0])
+  t2[1] = Math.round(t2[1])
+
+  newTime += t1[0] + t2[0];
+
+  let mins = t1[1] + t2[1];
+
+  while (mins > 60) {
+    newTime += 1;
+    mins -= 60;
+  }
+  newTime += ":" + mins;
+
+  return newTime
+}
+function subTimes(time1, time2) {
+  let newTime = 0;
+
+  let t1 = time1.split(":");
+  let t2 = time2.split(":");
+
+  t1[0] = Math.round(t1[0])
+  t1[1] = Math.round(t1[1])
+  t2[0] = Math.round(t2[0])
+  t2[1] = Math.round(t2[1])
+
+  newTime = t1[0] - t2[0];
+
+  let mins = t1[1] - t2[1];
+
+  while (mins < 0) {
+    newTime -= 1;
+    mins += 60;
+  }
+  newTime += ":" + mins;
+  return newTime;
+}
+function convertToReadableTime(hours, minutes) {
+  return hours + ":" + minutes;
+}
+function getTimeGap(time1, time2) {
+  return subTimes(time1, time2)
+}
--- 7-days.io/7-days.io/public/src/js/ttmanager.js
+++ 7-days.io/7-days.io/public/src/js/ttmanager.js
@@ -1,13 +1,39 @@
 ﻿const ttmanager = () => { document.querySelector("#editorField").style.display = "block"; }
 
+let days = 7;
+let periods = 6;
+
 let data = [];
 
-let field;
+let field = null;
+let fields = [];
+
+let shiftHeld = false;
+
+window.addEventListener("keydown", e => {
+  if (e.keyCode == 16) {
+    shiftHeld = true;
+  }
+})
+window.addEventListener("keyup", e => {
+  if (e.keyCode == 16) {
+    getUserData()
+  }
+})
 
 function editField(block) {
-  field = block;
-  document.querySelector(".getData").style.display = "block";
+  if (shiftHeld) {
+    fields.push(block);
+  } else {
+    fields = [block];
+    getUserData();
+  }
 }
+const getUserData = () => {
+  if (fields.length > 0)
+    document.querySelector(".getData").style.display = "block";
+}
+
 function parseData() {
   let dat = {
     name: document.querySelector("#name").value,
@@ -19,12 +45,38 @@ function parseData() {
 }
 
 const confirmEvent = () => {
-  // set value of innerHTML of field to recieved data
   let data = parseData();
 
-  field.innerHTML = "<B>" + data.name + "</B>" + "<br>" + data.loc;
-  field.setAttribute("from", data.from)
-  field.setAttribute("to", data.to)
+  for (let i = 0; i < fields.length; i++) {
+    fields[i].innerHTML = "<B>" + data.name + "</B>" + "<br>" + data.loc;
+
+    let period = fields[i].getAttribute("class")
+
+    for (let i = 0; i < document.getElementsByClassName(period).length; i++) {
+      document.getElementsByClassName(period)[i].setAttribute("from", data.from);
+      document.getElementsByClassName(period)[i].setAttribute("to", data.to);
+    }
+  }
+
+  doFinalCheck();
 
   document.querySelector(".getData").style.display = "none";
+
+  shiftHeld = false;
+  fields = [];
+  field = null;
+}
+
+function doFinalCheck() {
+  let day, period;
+
+  let timeGap = 0;
+
+  for (day = 1; day <= days; day++) {
+    for (period = 1; period < periods; period++) {
+      if (period > 1) {
+        // shift times so that they don't overlap
+      }
+    }
+  }
 }
